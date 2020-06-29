bcfb34468980b4313e682ea4560b7e6bd7c75d45
YeetmeisterII <germainjones1@gmail.com>
2020-06-29 12:51:26 +0100

    Create spells project.
    Additions:
        -Add base Spell class.
        -Add HealingSpell class.
        -Add OffensiveSpell class.

---
+++ spells/__init__.py
@@ -0,0 +1,34 @@
+from mediah.actions import Attack
+
+
+class Spell:
+    def __init__(self, name: str = 'Unknown magic', cost: int = 0):
+        self._name = name
+        self._mana_cost = cost
+
+    def is_castable(self, caster: 'Creature'):
+        if caster.magic_enabled() and (self._mana_cost <= caster.mana()):
+            return True
+        else:
+            return False
+
+    def cast(self, executor: 'Creature', target: 'Creature'):
+        pass
+
+
+class HealingSpell(Spell):
+    def __init__(self, health: int = 0, **kwargs):
+        super().__init__(**kwargs)
+        self._health = health
+
+    def cast(self, executor: 'Creature', target: 'Creature'):
+        pass
+
+
+class OffensiveSpell(Spell):
+    def __init__(self, damage: int = 0, **kwargs):
+        super().__init__(**kwargs)
+        self._damage = damage
+
+    def cast(self, executor: 'Creature', target: 'Creature'):
+        return Attack(executor, target, self._damage)
